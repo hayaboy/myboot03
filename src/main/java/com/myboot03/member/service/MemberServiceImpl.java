a89750d9eb44507db5651a04ccf0f64324de2b75
hayaboy <hayaboy@korea.ac.kr>
2023-04-20 12:29:24 +0900

    4_20

---
+++ src/main/java/com/myboot03/member/service/MemberServiceImpl.java
@@ -0,0 +1,61 @@
+package com.myboot03.member.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.myboot03.member.dao.MemberDAO;
+import com.myboot03.member.vo.MemberVO;
+
+
+@Service(value = "memberService")
+@Transactional(propagation = Propagation.REQUIRED)
+public class MemberServiceImpl implements MemberService{
+
+	@Autowired
+	private MemberDAO memberDAO;
+
+	/*
+	 * public void setMemberDAO(MemberDAO memberDAO) { this.memberDAO = memberDAO; }
+	 */
+
+
+
+	@Override
+	public List listMembers() throws DataAccessException {
+		List membersList = null;
+		membersList = memberDAO.selectAllMemberList();
+		return membersList;
+	}
+
+
+
+	@Override
+	public int addMember(MemberVO memberVO) throws DataAccessException {
+		int result =memberDAO.insertMember(memberVO);
+		return result;
+		
+	}
+
+
+	public void removeMember(String id ) throws DataAccessException{
+		memberDAO.deleteMember(id);
+	}
+
+
+
+	@Override
+	public MemberVO login(MemberVO memberVO) throws Exception {
+		return memberDAO.loginById(memberVO);
+	}
+
+
+
+
+	
+
+}
