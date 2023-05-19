6b36eefdf88d8478d49d2598d5751395fee3f5b0
hayaboy <hayaboy@korea.ac.kr>
2023-05-19 15:47:36 +0900

    5_19

--- src/test/java/com/myboot02/Myboot03ApplicationTests.java
+++ src/test/java/com/myboot02/Myboot03ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Myboot03ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
